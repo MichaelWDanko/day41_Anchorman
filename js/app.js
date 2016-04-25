0d4d0893b59e19bed65ea945a23cb91e070741fc
Michael Danko <MichaelDanko@icloud.com>
2016-04-25 13:52:38 -0400

    Have created the skelton for the app.js

--- js/app.js
+++ js/app.js
@@ -5,19 +5,48 @@ var angularRoute = require('angular-route');
 var app = angular.module('NewsFeedApp', ['ngRoute']);
 
 /*Create a router*/
-
-
-/*Create a service to store the data*/
-
-
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/feed', {
+        controller: 'FeedViewController',
+        templateUrl: 'sections/feed.html'
+    })
+    .when('/interests', {
+        controller: 'InterestViewController',
+        templateUrl: 'sections/interests.html'
+    })
+    .when('/saved', {
+        controller: 'SavedViewController',
+        templateUrl: 'sections/saved.html'
+    })
+    .otherwise({
+        controller: 'FeedViewController',
+        templateUrl: 'sections/feed.html'
+    });
+    
+}]);
 
 /*Create a controller for the Home feed page*/
-
+app.controller('FeedViewController', ['$scope', '$http', 'NewsService', function ($scope, $http, NewsService) {
+    console.log('FeedViewController View');
+}]);
 
 
 /*Create a controller for the Interests page*/
-
+app.controller('InterestViewController', ['$scope', '$http', 'NewsService', function ($scope, $http, NewsService) {
+    console.log('InterestViewController View');
+}]);
 
 
 /*Create a controller for the Saved page*/
+app.controller('SavedViewController', ['$scope', '$http', 'NewsService', function ($scope, $http, NewsService) {
+    console.log('SavedViewController View');
+}]);
 
+
+
+/*Create a service to store the data*/
+app.factory('NewsService', function ($http) {
+    /*Any data we need to store can go in here.*/
+    return;
+});
