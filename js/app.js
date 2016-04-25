269747ea211cc8ba6988fb695be9b5b3ca0699cc
Michael Danko <MichaelDanko@icloud.com>
2016-04-25 16:04:39 -0400

    Have the first list of stories appearing on the home page

--- js/app.js
+++ js/app.js
@@ -5,41 +5,46 @@ var angularRoute = require('angular-route');
 var app = angular.module('NewsFeedApp', ['ngRoute']);
 
 /*Create a router*/
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/feed', {
             controller: 'FeedViewController',
-            templateUrl: '/sections/feed.html'
+            templateUrl: 'sections/feed.html'
         })
         .when('/interests', {
             controller: 'InterestViewController',
-            templateUrl: '/sections/interests.html',
+            templateUrl: 'sections/interests.html',
         })
         .when('/saved', {
             controller: 'SavedViewController',
-            templateUrl: '/sections/saved.html',
+            templateUrl: 'sections/saved.html',
         })
         .otherwise({
-            controller: 'FeedViewController',
-            templateUrl: '/sections/feed.html'
+            redirectTo: '/feed',
         });
+    //        .otherwise({
+    //            controller: 'FeedViewController',
+    //            templateUrl: '/sections/feed.html'
+    //        });
 
 }]);
 
 /*Create a controller for the Home feed page*/
-app.controller('FeedViewController', ['$scope', '$http', 'NewsService', function($scope, $http, NewsService) {
+app.controller('FeedViewController', ['$scope', '$http', 'NewsService', function ($scope, $http, NewsService) {
     console.log('FeedViewController View');
+
+    $scope.articles = NewsService.getArticles();
 }]);
 
 
 /*Create a controller for the Interests page*/
-app.controller('InterestViewController', ['$scope', '$http', 'NewsService', function($scope, $http, NewsService) {
+app.controller('InterestViewController', ['$scope', '$http', 'NewsService', function ($scope, $http, NewsService) {
     console.log('InterestViewController View');
 }]);
 
 
 /*Create a controller for the Saved page*/
-app.controller('SavedViewController', ['$scope', '$http', 'NewsService', function($scope, $http, NewsService) {
+app.controller('SavedViewController', ['$scope', '$http', 'NewsService', function ($scope, $http, NewsService) {
     console.log('SavedViewController View');
 
 
@@ -48,14 +53,26 @@ app.controller('SavedViewController', ['$scope', '$http', 'NewsService', functio
 
 
 /*Create a service to store the data*/
-app.factory('NewsService', function($http) {
+app.factory('NewsService', function ($http) {
+
+    let stories = [];
+
     /*Any data we need to store can go in here.*/
     $http({
             method: 'get',
-            url: 'http://chat.queencityiron.com/api/news/latest'
+            url: 'http://chat.queencityiron.com/api/news/latest',
         })
-        .then(function(response) {
+        .then(function (response) {
             console.log(response);
+            angular.copy(response.data.stories, stories);
+            console.log('start');
+            console.log(stories);
         });
-    return;
+
+    return {
+        getArticles: function () {
+            return stories;
+        }
+    }
+
 });
