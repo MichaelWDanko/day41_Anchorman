f9ea8e69c92aba86bfb22dffdd5f04f9449b8082
Michael Danko <MichaelDanko@icloud.com>
2016-04-25 10:26:05 -0400

    Established the basic framework for the project

---
+++ js/app.js
@@ -0,0 +1,23 @@
+/* jslint browser: true */
+/* jslint esnext: true */
+var angular = require('angular');
+var angularRoute = require('angular-route');
+var app = angular.module('NewsFeedApp', ['ngRoute']);
+
+/*Create a router*/
+
+
+/*Create a service to store the data*/
+
+
+
+/*Create a controller for the Home feed page*/
+
+
+
+/*Create a controller for the Interests page*/
+
+
+
+/*Create a controller for the Saved page*/
+
